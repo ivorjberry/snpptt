27fcd4e13ff5d113c48ea5f0276b5af7e5e8ad14
Ivor Berry <ivorjberry@gmail.com>
2014-05-10 14:19:03 -0700

    inital commit
    na

---
+++ snpptt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(snpptt.Startup))]
+namespace snpptt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
